04d1bd2942e381357afa21e413662dbcc89b221d
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-03-15 17:59:57 +0900

    [Add] Initial commit.

---
+++ app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = (robot) => {
+    robot.hear(/hello>/i, (msg) => { // hello> を取得する正規表現
+        const username = msg.message.user.name;
+        msg.send('Hello, ' + username); // Hello,{username} とメッセージを送信する
+    });
+};
